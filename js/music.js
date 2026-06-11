7a8c489130436b5ae2cd3845b64ae4f26758bd79
yueming_dai <2637377691@qq.com>
2026-06-11 09:35:14 +0800

    Site updated: 2026-06-11 09:35:13

--- js/music.js
+++ js/music.js
@@ -1,10 +1,21 @@
 (function(){
-  var e=document.createElement("link");e.rel="stylesheet",e.href="https://cdn.jsdelivr.net/npm/aplayer@1/dist/APlayer.min.css",document.head.appendChild(e);
-  var s=document.createElement("script");s.src="https://cdn.jsdelivr.net/npm/aplayer@1/dist/APlayer.min.js",s.onload=function(){
-    var ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,mini:true,autoplay:false,theme:"#49b1f5",loop:"all",order:"random",preload:"auto",volume:.5,
-      audio:[{name:"晴天",artist:"周杰伦",url:"https://music.163.com/song/media/outer/url?id=186016.mp3",cover:"https://p2.music.126.net/2VQoEXJTmfDcS25CzH_Abw==/109951167817751443.jpg"},
-             {name:"夜曲",artist:"周杰伦",url:"https://music.163.com/song/media/outer/url?id=186001.mp3",cover:"https://p2.music.126.net/6cNXqILTHP1lZJIdOGMDRg==/109951167817755527.jpg"},
-             {name:"七里香",artist:"周杰伦",url:"https://music.163.com/song/media/outer/url?id=186019.mp3",cover:"https://p2.music.126.net/ZCHEFUQ3GKe3vJs1DhmWfQ==/109951167817750625.jpg"}]})},
+  var s=document.createElement("script");
+  s.src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js";
+  s.onload=function(){
+    var el=document.createElement("meting-js");
+    el.setAttribute("server","netease");
+    el.setAttribute("type","playlist");
+    el.setAttribute("id","8543070918");
+    el.setAttribute("fixed","true");
+    el.setAttribute("mini","true");
+    el.setAttribute("autoplay","false");
+    el.setAttribute("theme","#49b1f5");
+    el.setAttribute("loop","all");
+    el.setAttribute("order","random");
+    el.setAttribute("preload","auto");
+    el.setAttribute("volume","0.5");
+    document.getElementById("aplayer").appendChild(el);
+  };
   document.body.appendChild(s);
   var d=document.createElement("div");d.id="aplayer",document.body.appendChild(d);
 })();
